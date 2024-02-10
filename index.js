7dbc1431c0a2ce9ed5915e4865e572913a837fb6
ryansldev <ryanlima1309@gmail.com>
2024-02-10 11:10:00 -0300

    feat: player movement

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ class Player {
 
   update() {
     this.draw()
+    this.position.x += this.velocity.x
     this.position.y += this.velocity.y
 
     if(this.position.y + this.height + this.velocity.y <= canvas.height)
@@ -37,12 +38,65 @@ class Player {
 }
 
 const player = new Player()
+const keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  }
+}
 player.update()
 
 function animate() {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
   player.update()
+
+  if (keys.right.pressed) {
+    player.velocity.x = 5
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5
+  } else player.velocity.x = 0
 }
 
 animate()
+
+window.addEventListener('keydown', ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      console.log('left')
+      keys.left.pressed = true
+      break
+    case 83:
+      console.log('down')
+      break
+    case 68:
+      console.log('right')
+      keys.right.pressed = true
+      break
+    case 87:
+      console.log('up')
+      player.velocity.y -= 20
+      break
+  }
+})
+
+window.addEventListener('keyup', ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      console.log('left')
+      keys.left.pressed = false
+      break
+    case 83:
+      console.log('down')
+      break
+    case 68:
+      console.log('right')
+      keys.right.pressed = false
+      break
+    case 87:
+      console.log('up')
+      break
+  }
+})
